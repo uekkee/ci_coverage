7df0abdf7f440f164a164af0a2eb9fa769fdc22b
Susumu UEKIHARA <uekkee@gmail.com>
2021-10-20 15:57:56 +0900

    add untested codes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   belongs_to :author
 
   validates :title, presence: true
+
+  def untested_method
+    'hello world'
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   validates :name, presence: true
+
+  def untested_method
+    'dont think feel'
+  end
 end
