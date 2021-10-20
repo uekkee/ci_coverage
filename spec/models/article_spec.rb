c45e4925475894de56a687f0a1be947878bc8f71
Susumu UEKIHARA <uekkee@gmail.com>
2021-10-20 17:46:34 +0900

    try spec failure case

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   it { expect(described_class.new).to be_invalid }
+  it { expect(described_class.new(title: 'any')).to be_valid }
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it { expect(described_class.new).to be_invalid }
+  it { expect(described_class.new).to be_valid }
 end
